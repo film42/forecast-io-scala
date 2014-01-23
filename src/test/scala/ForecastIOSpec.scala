ca103a0111039f0b9a67d2910caa022bb7b26ec3
Garrett Thornburg <film42@gmail.com>
2014-01-23 10:46:37 -0700

    base test cases

--- src/test/scala/ForecastIOSpec.scala
+++ src/test/scala/ForecastIOSpec.scala
@@ -1,11 +1,91 @@
 package com.film42.forecastioapi
 
-import org.scalatest.FunSpec
+import org.scalatest._
+import com.film42.forecastioapi._
+import com.film42.forecastioapi.extras._
+import scala.util.Success
+import scala.util.Failure
+import java.util.Date
 
 class ForecastIOSpec extends FunSpec {
-  describe("Adding 1 to 1") {
-    it("should equals 2"){
-      assert(1+1 == 2)
+  val apiKey = System.getenv("FORECASTIO_APIKEY")
+
+  describe("Starting the test suite") {
+    it("should have a set apiKey system var"){
+      assert(apiKey != null)
+    }
+  }
+
+  describe("Test Locations") {
+    it("can find salt lake city UT") {
+      val Success(LocationPoint(lon, lat)) = Location.search("salt lake city ut")
+      assert(!lon.isEmpty)
+      assert(!lat.isEmpty)
+    }
+
+    it("returns blank LocationPoint when bad data given") {
+      val loc = Location.search("jfdowij0930jrf")
+      assert(loc.isInstanceOf[Failure[LocationPoint]])
+    }
+
+    it("can get a forecast with a LocationPoint") {
+      val loc = Location.search("salt lake city ut")
+      assert(loc.isInstanceOf[Success[LocationPoint]])
+      val Success(location) = loc
+
+      val resp = ForecastIO(apiKey).forecast(location)
+      assert(resp.isInstanceOf[Success[Forecast]])
+      val Success(forecast) = resp
+      assert(forecast.currently.summary != null)
+    }
+
+    it("can get a forecast with an older date") {
+      val loc = Location.search("salt lake city ut")
+      assert(loc.isInstanceOf[Success[LocationPoint]])
+      val Success(location) = loc
+
+      val date = new Date(1265076122 * 1000L)
+
+      val resp = ForecastIO(apiKey).forecast(location, date)
+      assert(resp.isInstanceOf[Success[Forecast]])
+      val Success(forecast) = resp
+      assert(forecast.currently.summary != null)
+      assert(forecast.datetime == date)
+    }
+  }
+
+  describe("Base ForecastIO things") {
+    it("will return error with a bad api key") {
+      val resp = ForecastIO("badkey").forecast("45.157778", "-93.226944")
+      assert(resp.isInstanceOf[Failure[Forecast]])
+    }
+
+    it("will return error with no lon / lat") {
+      val resp = ForecastIO(apiKey).forecast("", "")
+      assert(resp.isInstanceOf[Failure[Forecast]])
+    }
+
+    it("can get with lat / lon") {
+      val resp = ForecastIO(apiKey).forecast("45.157778", "-93.226944")
+      assert(resp.isInstanceOf[Success[Forecast]])
+      val Success(forecast) = resp
+      assert(forecast.currently.summary != null)
+    }
+
+    it("can get with lat / lon and date") {
+      val date = new Date(1265076122 * 1000L)
+      val resp = ForecastIO(apiKey).forecast("45.157778", "-93.226944", date)
+      assert(resp.isInstanceOf[Success[Forecast]])
+      val Success(forecast) = resp
+      assert(forecast.currently.summary != null)
+      assert(forecast.datetime == date)
+    }
+
+    it("can get with lat / lon and si units") {
+      val resp = ForecastIO(apiKey, "si").forecast("45.157778", "-93.226944")
+      assert(resp.isInstanceOf[Success[Forecast]])
+      val Success(forecast) = resp
+      assert(forecast.currently.summary != null)
     }
   }
 }
